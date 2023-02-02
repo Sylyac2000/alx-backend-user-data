b6b2388728582575c5e3f99934654b8efb66ce81
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-02 17:17:45 +0000

    1. Log formatter up

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module is about Regex-ing
 """
-from typing import List, Tuple
+from typing import List
 import re
 import logging
 
@@ -26,7 +26,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: Tuple[str, str, str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.__fields = fields
 
