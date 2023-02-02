3b383b6936be8770f66e0f93458ae52793f72997
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-02 17:13:39 +0000

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module is about Regex-ing
 """
-from typing import List
+from typing import List, Tuple
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -10,10 +11,30 @@ def filter_datum(fields: List[str],
                  message: str,
                  separator: str) -> str:
     """ return an obfuscated log message
-    regex [^;]+: one or more characters that are not a semicolon (;)
-    replacement = separator.join(f"{field}={redaction}" for field in fields)
     """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
                          field+'='+redaction+separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str, str, str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.__fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter and format logging message"""
+        log_msg = super(RedactingFormatter, self).format(record)
+        message = filter_datum(list(self.__fields),
+                               RedactingFormatter.REDACTION,
+                               log_msg,
+                               RedactingFormatter.SEPARATOR)
+        return message
