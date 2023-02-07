24eea35f70449d5c27048006e8abcdb51424c863
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-07 10:42:28 +0000

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from flask import request
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth(object):
     """Auth class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
@@ -31,7 +31,12 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
-        return None
+        if request is None:
+            return None
+        if not request.headers.get('Authorization'):
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user"""
